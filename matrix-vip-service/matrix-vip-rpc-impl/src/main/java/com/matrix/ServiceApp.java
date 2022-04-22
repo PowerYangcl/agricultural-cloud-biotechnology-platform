45e84ba5dfff544b17ffedd3b7520a1f6ca5074a
yangchenglin <yangchenglin11@jd.com>
2022-04-22 14:48:40 +0800

    初始化与结构调整。

--- matrix-vip-service/matrix-vip-rpc-impl/src/main/java/com/matrix/ServiceApp.java
+++ matrix-vip-service/matrix-vip-rpc-impl/src/main/java/com/matrix/ServiceApp.java
@@ -2,22 +2,12 @@ package com.matrix;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import com.matrix.system.init.SystemInit;
 
 import java.io.IOException;
 
 public class ServiceApp {
 
     public static void main(String[] args) throws IOException {
-        /*ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[]{
-        		"classpath:META-INF/spring/spring-mongo.xml",
-                "classpath:META-INF/spring/applicationContext.xml", 
-                "classpath:META-INF/spring/spring-dubbo.xml",
-                "classpath:META-INF/spring/spring-mybatis.xml", 
-                "classpath:META-INF/spring/spring-cache.xml", 
-                "classpath:META-INF/spring/spring-data-redis.xml"});*/
-        
-        
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[]{
                 "classpath*:applicationContext.xml",  
                 });
